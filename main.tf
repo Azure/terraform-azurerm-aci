97ff624001e2e6c9df996ad3f83baaf0c34e29a0
Thorsten Hans <thorsten.hans@gmail.com>
2020-10-02 21:36:09 +0200

    chore: use Terraform 0.12 syntax

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 resource "azurerm_resource_group" "aci_rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_container_group" "containergroup" {
-  name                  = "${var.container_group_name}"
-  resource_group_name   = "${azurerm_resource_group.aci_rg.name}"
-  location              = "${azurerm_resource_group.aci_rg.location}"
-  ip_address_type       = "public"
-  dns_name_label        = "${var.dns_name_label}"
-  os_type               = "${var.os_type}"
+  name                = var.container_group_name
+  resource_group_name = azurerm_resource_group.aci_rg.name
+  location            = azurerm_resource_group.aci_rg.location
+  ip_address_type     = "public"
+  dns_name_label      = var.dns_name_label
+  os_type             = var.os_type
 
   container {
-      name      = "${var.container_name}"
-      image     = "${var.image_name}"
-      cpu       = "${var.cpu_core_number}"
-      memory    = "${var.memory_size}"
-      port      = "${var.port_number}"
+    name   = var.container_name
+    image  = var.image_name
+    cpu    = var.cpu_core_number
+    memory = var.memory_size
+    port   = var.port_number
   }
 }
